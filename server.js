0a9709f20dadb5dde9cbbbcb37512a6e581cdcd3
Simone de Blasiis <simone.deblasiis@gmail.com>
2018-01-25 16:18:14 +0100

    Prova deplyment su Heroku

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
+
+    var router = express.Router();
+
+//test router
+    router.get('/', function (req, res) {
+        res.send('Prova deployment');
+    });
+
+    app.use(router);
+var port = 8085;
+    if(process.env.NODE_ENV !== 'test') {
+        port = 8080;
+    }
+
+    var server = app.listen(port, 'localhost',  function () {
+        if(process.env.NODE_ENV !== 'test') {
+            console.log("Service attivo sulla porta 8080 e ascolta solo per localhost");
+        }
+    });
